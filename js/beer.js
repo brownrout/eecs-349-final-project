afa5b3f6bd3bf53e5090222ea4b3dc47526646a7
brownrout <ericbrownrout2016@u.northwestern.edu>
2016-06-08 22:10:15 -0500

    first draft

---
+++ js/beer.js
@@ -0,0 +1,16 @@
+
+function cityChange(x) {
+// Called when selects from 
+   document.getElementById("city-dropdown").innerHTML = x;
+}
+
+function loadMore(x) {
+// Called when selects from 
+   document.getElementById("load").innerHTML = x;
+}
+
+function signUps(x) {
+// Called when selects from 
+   document.getElementById("replace").innerHTML = x;
+   return false;
+}
